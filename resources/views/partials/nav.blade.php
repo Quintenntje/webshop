a40b8c5e0d5a3fd7fe3f3768892df9903a2b8a7c
Quinten <quintenclaes7@gmail.com>
2025-10-07 22:43:39 +0200

    feat: update navigation layout and logo styling for improved responsiveness

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,5 @@
 <header class="header">
-  <div class="container header__items">
+  <div class="container header__items mobile-only">
     <div class="header__items__left">
       <button class="btn btn--transparent header__items__left__button">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg>
@@ -13,7 +13,7 @@
     </div>
     <div class="header__items__center">
         <a href="/" class="link link--nav header__items__center__logo">
-            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="w-10 h-10">
+            <img class="nav__logo" src="{{ asset('images/logo/coolKickslogo.png') }}" alt="Logo" >
         </a>
     </div>
     <div class="header__items__right">
@@ -21,4 +21,31 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-handbag-icon lucide-handbag"><path d="M2.048 18.566A2 2 0 0 0 4 21h16a2 2 0 0 0 1.952-2.434l-2-9A2 2 0 0 0 18 8H6a2 2 0 0 0-1.952 1.566z"/><path d="M8 11V6a4 4 0 0 1 8 0v5"/></svg>
         </a>
   </div>
+</div>
+<div class="container header__items pc-only">
+
+  <div class="header__items__left">
+    <a href="/" class="link link--nav header__items__center__logo">
+      <img class="nav__logo" src="{{ asset('images/logo/coolKickslogo.png') }}" alt="Logo" >
+  </a>
+    </div>
+    <nav class="header__items__center">
+      <ul class="header__items__center__nav">
+        <li class="header__items__center__nav__item">
+          <a href="/" class="link link--nav header__items__center__nav__item__link">Home</a>
+        </li>
+      </ul>
+    </nav>
+  <div class="header__items__right">
+    <a href="/search" class="link link--nav header__items__left__icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+    </a>
+    <a href="account" class="link link--nav header__items__left__icon">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+    </a>
+    <a href="/cart" class="link link--nav nav__items__right__button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-handbag-icon lucide-handbag"><path d="M2.048 18.566A2 2 0 0 0 4 21h16a2 2 0 0 0 1.952-2.434l-2-9A2 2 0 0 0 18 8H6a2 2 0 0 0-1.952 1.566z"/><path d="M8 11V6a4 4 0 0 1 8 0v5"/></svg>
+    </a>
+  </div>
+
 </header>
