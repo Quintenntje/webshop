e815b6b77e6a65597b165070283deea7a2e1e71a
Quinten <quintenclaes7@gmail.com>
2025-10-09 12:44:21 +0200

    fix: correct HTML structure in mobile navigation menu

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -70,7 +70,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
     </button>
   </div>
-  <ul class="mobile-menu__links">p
+  <ul class="mobile-menu__links">
     <li><a class="link link--nav" href="/shop">Shop</a></li>
     <li><a class="link link--nav {{ request()->is('shoes/men') ? 'active' : '' }}" href="/shoes/men">Men</a></li>
     <li><a class="link link--nav {{ request()->is('shoes/women') ? 'active' : '' }}" href="/shoes/women">Women</a></li>
