8773215fba313ce645b699a7713d9601f327e0c2
Quinten <quintenclaes7@gmail.com>
2025-10-09 12:22:04 +0200

    feat: update shoes list view to display gender-specific title and product count

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -1,10 +1,14 @@
 <x-layout>
-    <h1>Shoes List</h1>
+    <div class="container">
+    <h1> {{ $gender->name }} Shoes</h1>
+    <p>{{ $products->count() }} results</p>
 
     @foreach ($products as $product)
         <h2>{{ $product->name }}</h2>
         <p>{{ $product->price }}</p>
+        @endforeach
+    </div>
 
-
-    @endforeach
+    
+   
 </x-layout>
