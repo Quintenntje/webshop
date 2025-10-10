993e2ca9849b7d31beedc0d927d2f4f8526cb74a
Quinten <quintenclaes7@gmail.com>
2025-10-10 16:18:22 +0200

    feat: enhance product card styling with hover effects, add product description, and adjust layout for improved presentation

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -8,7 +8,11 @@
     </div>
     <div class="product-card__content">
         <h3 class="product-card__title">{{ $product->name }}</h3>
-        <p class="product-card__price">{{ $product->price }}</p>
+        <div class="product-card__description">
+       <p>{{ $product->gender->name }} Shoes</p>
+        <p>{{ $product->brand->name }}</p>
+        </div>
+        <p class="product-card__price">€{{ $product->price }}</p>
     </div>
     <x-link href="/products/{{ $product->id }}" color="full" size=""><span class="sr-only">View product</span></x-link>
 </article>
