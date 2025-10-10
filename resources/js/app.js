984654324658109a174e484a683172b5bd73fd6c
Quinten <quintenclaes7@gmail.com>
2025-10-10 13:41:13 +0200

    refactor using vite for css and js, disabled actions due sending email on failed job

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import initNav from './nav';
+
+document.addEventListener('DOMContentLoaded', () => {
+    initNav();
+});
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -23,5 +22,4 @@
            {{ $slot }}
         </main>
     </body>
-    <script src="{{ asset('js/main.js') }}"></script>
 </html>
