02055631486f6615f27c8f0d03ab37b20fa65786
Quinten <quintenclaes7@gmail.com>
2025-10-12 23:00:11 +0200

    feat:  added icon in button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,6 +2,7 @@
     'type' => 'button',
     'color' => 'primary',
     'size' => 'md',
+    'icon' => false,
 ])
 
 @php
@@ -9,6 +10,8 @@ $classes = match($color) {
     'primary' => 'btn btn--primary btn--'.$size,
     'secondary' => 'btn btn--secondary btn--'.$size,
     'danger' => 'btn btn--danger btn--'.$size,
+    'icon' => 'btn btn--icon btn--'.$size,
+    'icon-danger' => 'btn btn--icon-danger btn--'.$size,
     default => 'btn btn--primary btn--'.$size,
 };
 @endphp
