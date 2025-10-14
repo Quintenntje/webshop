01d53a6e381a73adb75a987e0f66eca883622ab7
Quinten <quintenclaes7@gmail.com>
2025-10-14 13:58:18 +0200

    feat: add password input component with toggle visibility button

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -15,4 +15,15 @@ $classes = match($color) {
     default => 'input input--primary',
 };
 @endphp
-<input type="{{ $type }}" name="{{ $name }}" placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }} {{ $attributes->merge(['class' => "$classes"]) }}>
+
+@if ($type === 'password')
+    <div class="input input__password ">
+        <input type="{{ $type }}" name="{{ $name }}" placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }} >
+        <x-button type="button" color="transparent" size="sm" class="input__password__button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+        </x-button>
+    </div>
+
+    @else
+    <input type="{{ $type }}" name="{{ $name }}" placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }} {{ $attributes->merge(['class' => "$classes"]) }}>
+    @endif
