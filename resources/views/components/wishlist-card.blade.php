0e2a0f67cfc3a77a3926953ad090948a8117c2f5
Quinten <quintenclaes7@gmail.com>
2025-10-16 09:52:12 +0200

    feat: enhance wishlist card styling and structure with new CSS variables and layout adjustments

--- resources/views/components/wishlist-card.blade.php
+++ resources/views/components/wishlist-card.blade.php
@@ -4,7 +4,7 @@
 
 <div class="wishlist-card">
     <div class="wish-list-heart">
-        <form action="{{ route('wishlist.remove') }}" method="POST">
+        <form class="wish-list-heart__form" action="{{ route('wishlist.remove') }}" method="POST">
             @csrf
             <input type="hidden" name="product_variant_id" value="{{ $product->id }}">
             <x-button type="submit" name="remove-from-wishlist" value="remove-from-wishlist" color="transparent" size="sm"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-icon lucide-heart"><path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"/></svg></x-button>
@@ -14,8 +14,8 @@
         <img src="{{ $product->product->primaryImage->filename }}" alt="{{ $product->name }}">
     </div>
     <div class="wishlist-card__content">
-        <h3 class="wishlist-card__title">{{ $product->name }}</h3>
-        <p class="wishlist-card__price">€{{ $product->price }}</p>
+        <h3 class="wishlist-card__title">{{ $product->product->name }}</h3>
+        <p class="wishlist-card__price">€{{ $product->product->price }}</p>
     </div>
     <form action="{{ route('cart.add') }}" method="POST">
         @csrf
