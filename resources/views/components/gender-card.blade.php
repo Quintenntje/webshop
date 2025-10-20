18d19d357bf04f16860b7725590cf6f813aa9594
Quinten <quintenclaes7@gmail.com>
2025-10-20 15:03:30 +0200

    fix: modify gender card call-to-action structure for improved layout and semantics

--- resources/views/components/gender-card.blade.php
+++ resources/views/components/gender-card.blade.php
@@ -4,7 +4,7 @@
     <div class="gender-card__overlay"></div>
     <div class="gender-card__content">
         <h3 class="gender-card__title">{{ $gender->name }}</h3>
-        <p class="gender-card__cta">Shop Now <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-icon lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg></p>
+        <div class="gender-card__cta"> <p>Shop Now</p> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-icon lucide-arrow-right"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg></d>
     </div>
 </a>
 
