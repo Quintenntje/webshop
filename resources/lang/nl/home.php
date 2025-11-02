f1aad06d37ce8280b1d5f9db6143a57f84378037
Quinten <quintenclaes7@gmail.com>
2025-11-02 14:50:26 +0100

    fix: update Dutch localization for shoe-related terms to enhance accuracy and clarity

--- resources/lang/nl/home.php
+++ resources/lang/nl/home.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'hero_title' => 'Een revolutie in schoeisel',
-    'hero_description' => 'De toekomst van schoeisel is hier',
+    'hero_title' => 'Een revolutie in schoenen',
+    'hero_description' => 'De toekomst van schoenen is hier',
     'shop_now' => 'Shop Nu',
     'shop_by_gender' => 'Shop op Geslacht',
     'shop_by_brand' => 'Shop op Merk',
