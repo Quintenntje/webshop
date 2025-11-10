90be17676788e9a858bbcafd998ea81cab3a7e9d
Quinten <quintenclaes7@gmail.com>
2025-11-10 02:12:56 +0100

    feat: customer to filament

---
+++ app/Filament/Resources/Customers/Schemas/CustomerForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Customers\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class CustomerForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('first_name')
+                    ->required(),
+                TextInput::make('last_name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                TextInput::make('phone_number')
+                    ->tel()
+                    ->default(null),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+                TextInput::make('role_id')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Customers/Schemas/CustomerInfolist.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Customers\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class CustomerInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('first_name'),
+                TextEntry::make('last_name'),
+                TextEntry::make('email')
+                    ->label('Email address'),
+                TextEntry::make('phone_number')
+                    ->placeholder('-'),
+                TextEntry::make('role_id')
+                    ->numeric(),
+                TextEntry::make('created_at')
+                    ->dateTime(),
+                TextEntry::make('updated_at')
+                    ->dateTime(),
+            ]);
+    }
+}
