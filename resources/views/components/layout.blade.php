c3cb4ce55342bffc78b2e752c7f8a4c98fee657c
Quinten <quintenclaes7@gmail.com>
2026-04-27 20:08:39 +0200

    feat: add analytics script

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
         {!! \Artesaos\SEOTools\Facades\SEOTools::generate() !!}
 
         <!-- Styles / Scripts -->
+        <script defer src="https://analytics.quintenclaes.be/script.js" data-website-id="1610ff79-51f0-4c0c-aef0-13c39dd2ecc3"></script>
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
